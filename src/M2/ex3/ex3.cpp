0bf500978268ad78829dba69382ab7147c085c77
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2025-05-14 20:51:52 -0300

    vértices no ex3 atualizados

--- src/M2/ex3/ex3.cpp
+++ src/M2/ex3/ex3.cpp
@@ -102,7 +102,7 @@ int main()
 	glViewport(0, 0, width, height);
 
 	GLuint shaderID = setupShader();
-	GLuint VAO = createTriangle(-0.5,-0.5,0.5,-0.5,0.0,0.5);
+	GLuint VAO = createTriangle(-0.1,-0.1,0.1,-0.1,0.0,0.1);
 
 	glUseProgram(shaderID);
 
