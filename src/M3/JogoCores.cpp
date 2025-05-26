4ebea4e35c11ad4f7b1f3f624c41c1ff7917a5ef
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2025-05-26 20:05:50 -0300

    Add color game implementation and update build files

---
+++ src/M3/JogoCores.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <vector>
+
+using namespace std;
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace glm;
+
+#include <cmath>
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+GLuint createQuad();
+int setupShader();
+int setupGeometry();
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint QUAD_WIDTH = 100, QUAD_HEIGHT = 100;
+const GLuint COLS = WIDTH / QUAD_WIDTH;
+const GLuint ROWS = HEIGHT / QUAD_HEIGHT;
+
+const GLchar *vertexShaderSource = R"(
+#version 400
+layout (location = 0) in vec3 position;
+uniform mat4 projection;
+uniform mat4 model;
+void main()	
+{
+	gl_Position = projection * model * vec4(position.x, position.y, position.z, 1.0);
+}
+)";
+
+const GLchar *fragmentShaderSource = R"(
+#version 400
+uniform vec4 inputColor;
+out vec4 color;
+void main()
+{
+	color = inputColor;
+}
+)";
+
+struct Quad
+{
+	vec3 position;
+	vec3 dimensions;
+	vec3 color;
+};
+
+vector <vec3> colors;
+int iColor = 0;
+
+int main()
+{
+	srand(glfwGetTime());
+
+	glfwInit();
+
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Jogo das Cores", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+	}
+
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+	const GLubyte *version = glGetString(GL_VERSION);
+	cout << "Renderer: " << renderer << endl;
+	cout << "OpenGL version supported " << version << endl;
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	GLuint shaderID = setupShader();
+
+	glUseProgram(shaderID);
+
+	GLint colorLoc = glGetUniformLocation(shaderID, "inputColor");
+
+	mat4 projection = ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
+	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
+
+	Quad grid[ROWS][COLS];
+	for(int i = 0; i < ROWS; i++) {
+	    for (int j = 0; j < COLS; j++) {
+	        Quad quad;
+	        quad.position = vec3(j * QUAD_WIDTH, i * QUAD_HEIGHT, 0.0);
+	        quad.dimensions = vec3(QUAD_WIDTH, QUAD_HEIGHT, 1.0);
+	        quad.color = vec3(rand() % 256 / 255.0, rand() % 256 / 255.0, rand() % 256 / 255.0);
+	        grid[i][j] = quad;
+	    }
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glLineWidth(10);
+		glPointSize(20);
+
+
+		glBindVertexArray(createQuad());
+
+		for (int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLS; j++) {
+                mat4 model = mat4(1);
+                model = translate(model, grid[i][j].position);
+                model = scale(model, grid[i][j].dimensions);
+                glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
+
+				glUniform4f(colorLoc, 
+					grid[i][j].color.r, 
+					grid[i][j].color.g, 
+					grid[i][j].color.b,
+					1.0f);
+				glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+			}
+		}
+
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int setupShader()
+{
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		cout << xpos << "  " << ypos << endl;	
+	}
+}
+
+GLuint createQuad()
+{
+	GLuint VAO;
+
+    GLfloat vertices[] = {
+		0.0f, 0.0f, 0.0f,
+        0.0f, 100.0f, 0.0f,
+        100.0f, 0.0f, 0.0f,
+        100.0f,100.0f, 0.0f
+	};
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	return VAO;
+}
